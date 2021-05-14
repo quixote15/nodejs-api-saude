536240a2612ea7f7bc1bde3ad8d5633ff5aef785
quixote15 <santos2tiago@gmail.com>
2021-05-13 21:55:37 -0300

    feature: add mongo to docker compose

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose';
 class Database {
   static async connect() {
-      const connectionString =  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ayo-lrkjt.gcp.mongodb.net/${process.env.MONGODB_NAME}?retryWrites=true&w=majority`;
+      const {
+        MONGODB_NAME,
+        MONGODB_PASSWORD,
+        MONGODB_PASSWORD,
+        MONGODB_HOST,
+      } =  process.env
+      const connectionString =  `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_NAME}`;
       
       mongoose.Promise = global.Promise;
       mongoose.connect(connectionString, { 
